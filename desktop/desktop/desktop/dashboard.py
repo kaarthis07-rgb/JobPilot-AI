b1039b50d03cfd54f6c29a3b62ec8a73484d12b1
kaarthis07-rgb <kaarthis07@gmail.com>
2026-07-26 15:14:41 +0530

    Delete desktop/desktop directory

--- desktop/desktop/desktop/dashboard.py
+++
@@ -1,7 +0,0 @@
-from PySide6.QtWidgets import QWidget
-
-class Dashboard(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.setWindowTitle("JobPilot AI Dashboard")
-        self.resize(1000, 700)
